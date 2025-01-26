f87b78f262e1aa7bdc8aff03033870aa01fa99e5
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-01-25 23:22:55 -0500

    secret key changed

--- gtmoviestore/gtmoviestore/settings.py
+++ gtmoviestore/gtmoviestore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2-$jjg$$d9p%gw$df=6no0h9o%^^=_%rt*e)-eods@$-9!9p$6'
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
