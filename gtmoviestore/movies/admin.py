997c40ae244a27279915ca08acc91cf4e7225537
mjlee041 <60720024+mjlee041@users.noreply.github.com>
2025-02-03 13:58:06 -0500

    Add files via upload

--- gtmoviestore/movies/admin.py
+++ gtmoviestore/movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Movie, Review
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
--- gtmoviestore/movies/views.py
+++ gtmoviestore/movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Movie
+
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Movies'
+    template_data['movies'] = Movie.objects.all()
+    return render(request, 'movies/index.html',
+                  {'template_data': template_data})
 
 # Create your views here.
