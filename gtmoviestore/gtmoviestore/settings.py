f5fd55539eae99e5589887aacd40d83440e73a23
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-02-04 12:52:56 -0500

    fixed movie image configuration and formatting

--- gtmoviestore/gtmoviestore/settings.py
+++ gtmoviestore/gtmoviestore/settings.py
@@ -135,3 +135,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "gtmoviestore/static/",
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
