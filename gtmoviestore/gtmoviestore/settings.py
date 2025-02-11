fd161111d10fd51beb6aa79e3ab6a63793d53543
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-02-11 17:56:36 -0500

    reset password functionality complete

--- gtmoviestore/gtmoviestore/settings.py
+++ gtmoviestore/gtmoviestore/settings.py
@@ -137,3 +137,11 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ="gtmoviestorereset@gmail.com" # Your Gmail address
+EMAIL_HOST_PASSWORD = "zbez urky jytm cafu"  # App Password
